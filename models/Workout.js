d2c217c42e05be805b0d2278fd8c27a1414a9067
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-05-10 00:32:53 -0700

    made notes not required

--- models/Workout.js
+++ models/Workout.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
   name: { type: String, required: true, trim: true, minLength: 1 },
-  notes: { type: String, required: true, trim: true, maxLength: 500 },
+  notes: { type: String, trim: true, maxLength: 500 },
   exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise" }],
   sessions: [{ type: Schema.Types.ObjectId, ref: "Session" }],
   user: { type: Schema.Types.ObjectId, ref: "User" },
